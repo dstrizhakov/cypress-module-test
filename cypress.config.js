4adb7c533544577ba419278366b44ac3132297db
Dmitry Strizhakov <d.strizhakov@gmail.com>
2023-07-21 11:10:08 +0300

    fix: remove dead code

--- cypress.config.js
+++ cypress.config.js
@@ -1,19 +1,15 @@
 import { defineConfig } from "cypress";
 import path from "path";
 
-// import { fileURLToPath } from "url";
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-
 const CY_TEMPLATE = process.env.CY_TEMPLATE || "./dist"; // path to the template folder
 const CY_TEST_CASES = process.env.CY_TEST_CASES || "./dist"; // path to the test cases
 const CY_TEST_HOST = process.env.CY_TEST_HOST || "http://127.0.0.1:5500/dist"; // path to the test host wheare index.html hosted
-const CY_VIDEO_COMPRESSION = process.env.CY_TEST_HOST || -1; //-1 disabled by default, 0 best quality, min compression; 100 worst quality, max compression
+const CY_VIDEO_COMPRESSION = process.env.CY_VIDEO_COMPRESSION || -1; //-1 disabled by default, 0 best quality, min compression; 100 worst quality, max compression
 
 const isVideo = CY_VIDEO_COMPRESSION >= 0 && CY_VIDEO_COMPRESSION <= 100;
 let videoQuality = 0;
 if (isVideo) {
-  videoQuality = Math.floor(CY_VIDEO_COMPRESSION*51/100) 
+  videoQuality = Math.floor((CY_VIDEO_COMPRESSION * 51) / 100);
 }
 
 export default defineConfig({
@@ -43,8 +39,8 @@ export default defineConfig({
 
     //A glob pattern String or Array of glob pattern Strings of the spec files to load.
     specPattern: path.join(CY_TEST_CASES, "tests/e2e/**/*.cy.{js,jsx,ts,tsx}"), //"./dist/tests/e2e/**/*.cy.{js,jsx,ts,tsx}",
-    
-    // Path to file to load before spec files load. This file is compiled and bundled. 
-    supportFile: false, 
+
+    // Path to file to load before spec files load. This file is compiled and bundled.
+    supportFile: false,
   },
 });
